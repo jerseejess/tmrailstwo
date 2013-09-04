5de21186737a802347c1fc42c437aa9630135d92
Jessica Lin <jessica.lin8@gmail.com>
2013-09-04 19:01:48 -0400

    Add Image Uploading via URL

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -11,4 +11,9 @@ class Pin < ActiveRecord::Base
 															size: { less_than: 5.megabytes }
 
 	belongs_to :user
+
+	def image_remote_url=(url_value)
+		self.image = URI.parse(url_value) unless url_value.blank?
+		super 
+	end
 end
