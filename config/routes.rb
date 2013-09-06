508453787aa224a99ef0d2f2c0d9497ea8291bbc
Jessica Lin <jessica.lin8@gmail.com>
2013-09-05 22:50:20 -0400

    Add User show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Tmrails::Application.routes.draw do
+  get "users/show"
+
   resources :pins
 
   devise_for :users
+  get 'users/:id' => 'users#show', as: :user
+
   get 'about' => 'pages#about'
 
   root :to => 'pins#index'
